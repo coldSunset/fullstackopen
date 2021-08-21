74d40330d5170710ee5ae535a6c7c8af95de98f2
Ravindu Sirimanna <ravsiriman@gmail.com>
2021-08-22 09:23:04 +1000

    Component States

---
+++ part 1/1b/Component State/componentstate/src/index.js
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  )
+
+
+
+
+
+/*
+setInterval(() => {
+  refresh();
+  counter +=1; 
+}, 1000);
+
+
+refresh(); 
+counter += 1; 
+refresh(); 
+counter += 1; 
+refresh(); 
+*/
