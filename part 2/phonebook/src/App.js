da72761b537f502657bd8021dd929b2fd40aac31
Ravindu Sirimanna <ravsiriman@gmail.com>
2021-08-24 22:26:00 +1000

    2c

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,23 +1,71 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const App=() => {
-  const [persons, setPersons] = useState([
-    { name: 'Eric Blair', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+  const [search, setSearch ] = useState(false)
+  const [filterArray, setFilterArray] = useState(persons.map(person=>person.name))
 
+useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response=>{
+      console.log('promised fulfilled')
+      setPersons(response.data)
+    })
+}, [])
 
-  const Person = ({person}) => {
-    return (
+  console.log('render', persons.length, 'persons')
+
+  const Contacts = ({person}) => {
+
+    return(
       <div>{person.name} {person.number}</div>
     )
   }
+
+  const Person = ({persons}) => {
+    if(search){
+    persons = persons.filter(per=>filterArray.includes(per.name))
+    }
+    return (
+      persons.map(person=><Contacts key={person.name} person={person}/>)
+    )
+  }
+
+  const PersonForm = () => {
+
+    return(
+      <form onSubmit={insertName}>
+      <div>
+        name:<input value={newName} onChange={handleNameChange}/>
+      </div>
+      <div>
+        number: <input value = {newNumber} onChange={handleNumberChange}/>
+      </div>
+      <div>
+      <button type='submit'>add</button>
+      </div>
+    </form>
+    )
+  }
+
+  const Filter = () => {
+
+    return(
+      <p>
+      filter shown with<input value={newFilter} onChange={handleFilterChange}/>
+    </p>
+    )
+  }
+
+  // event handlers 
 
   const insertName = (event) => {
     event.preventDefault()
@@ -28,7 +76,10 @@ const App=() => {
       number: newNumber
     }
     setPersons(persons.concat(newPerson))
+    setFilterArray(persons.map(person=>person.name))
+    console.log('check new', persons)
     setNewName('')
+    setNewNumber('')
     }
     else{
       alert(`${newName} is already added to phonebook`)
@@ -42,31 +93,51 @@ const App=() => {
   const handleNumberChange = (event) =>{
     //console.log(event.target.value)
     setNewNumber(event.target.value)
+
   }
 
   const handleFilterChange = (event) =>{
     setNewFilter(event.target.value)
+    if(event.target.value !==""){
+    setSearch(true)
+    const filter= event.target.value
+    const people=persons.map(person=>person.name)
+    //console.log('new people array',people.filter(str=>str.includes(filter)))
+    setFilterArray(people.filter(str=>str.includes(filter)))
+  }
+  else{
+    setSearch(false)
+    setFilterArray(persons.map(person=>person.name))
+  }
+  console.log('State of filter',filterArray)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+
       <p>
-        filter shown with<input/></p>
+      filter shown with<input value={newFilter} onChange={handleFilterChange}/>
+    </p>
+
+      <h3>Add a new</h3>
+
       <form onSubmit={insertName}>
-        <div>
-          name:<input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value = {newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-        <button type='submit'>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
       <div>
-        {persons.map(person=><Person key={person.name} person={person}/>)}
+        name:<input value={newName} onChange={handleNameChange}/>
+      </div>
+      <div>
+        number: <input value = {newNumber} onChange={handleNumberChange}/>
+      </div>
+      <div>
+      <button type='submit'>add</button>
+      </div>
+    </form>
+
+      <h3>Numbers</h3>
+      
+      <div>
+        <Person persons={persons}/>
       </div>
       <div><strong>debug: {newName} {newNumber}</strong></div>
     </div>
